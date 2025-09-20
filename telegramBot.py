576d35c01bbafa2583b593ac64ee6add456260b0
samWidlund <sam.wid415@gmail.com>
2025-09-20 17:17:37 +0200

    fix some comments and notification text

--- telegramBot.py
+++ telegramBot.py
@@ -4,25 +4,30 @@ from dotenv import load_dotenv
 import requests
 import sys
 
+# load enviroment variables
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("BOT_CHAT_ID")
 
 def notify_product(title, price, url):
+
+    # stop if tokens not valid
     if not TOKEN or not CHAT_ID:
-        print("Fel: BOT_TOKEN eller BOT_CHAT_ID är inte satta som miljövariabler")
+        print("error: BOT_TOKEN or BOT_CHAT_ID could not be found as environment variables")
         return False
-    
-    text = f"🔥 Ny produkt hittad!\n\n{title}\nPris: {price} kr\n{url}"
+
+    ## test notification
+    text = f"🚨 Product found! 🚨\n{title}\n{price} sek\n{url}"
     url_api = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    ## test notification
     
     try:
         r = requests.post(url_api, data={"chat_id": CHAT_ID, "text": text}, timeout=10)
-        r.raise_for_status()  # throw exception when http error
+        r.raise_for_status() # throw exception when http error
         
         response = r.json()
         if response.get("ok"):
-            print("message sent!")
+            print("notification sent!")
             return True
         else:
             print(f"wrong answer from Telegram API: {response}")
