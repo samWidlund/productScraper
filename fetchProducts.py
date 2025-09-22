54a66f6e0e3c5d8b64242012e34989570fc672e1
samWidlund <sam.wid415@gmail.com>
2025-09-22 15:03:10 +0200

    create file fetchProducts.py with inital script included

---
+++ fetchProducts.py
@@ -0,0 +1,25 @@
+# script to fetch products form platforms n notify user via telegramBot
+
+# https://github.com/kyleronayne/marketplace-api
+# https://github.com/scrapy/scrapy
+# https://github.com/D4Vinci/Scrapling
+
+import requests
+from dotenv import load_dotenv
+from telegramBot import notify_product
+
+load_dotenv()
+
+runProgram = True
+notify = "False"
+# json placeholder
+response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+print("Mock data:", response.json())
+
+while runProgram:
+    notify = input("Enter true to send message\n").lower()
+    print(notify)
+    if notify == "true":
+        notify_product("GOOD SHIT!", "500 kr", "https://example.com")
+    if notify == "break":
+        runProgram = False 
