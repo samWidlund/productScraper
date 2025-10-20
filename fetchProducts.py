36f3c58d4dbdd408ee6fe619d6b84a6dd2e8af96
samWidlund <sam.wid415@gmail.com>
2025-10-20 20:12:05 +0200

    working notifictation with price cap statement, also working debug state

--- fetchProducts.py
+++ fetchProducts.py
@@ -16,11 +16,14 @@ products = ebay.search(
     max_price=100000,
     marketplace='US'
 )
+debug = False
+price_cap = 19 # notification is sent to user when found product meets or is below this price
 
 print("\nproducts found:")
 for product in products:
-    if product['title'].startswith('Kids Smart Phone With 2.8"'):
-        print(f"{product['title']} - {product['price']} {product['currency']} {product['url']}")
-        notify_product(product['title'], product['price'], product['url'])
 
-# notify_product("test", "100", "test.com")
+    if debug: # if set to True, print all products found
+        print(f"\n{product['title']} - {product['price']} {product['currency']} {product['url']}")
+
+    if not debug and float(product['price']) <= price_cap:
+        notify_product(product['title'], product['price'], product['url'])
