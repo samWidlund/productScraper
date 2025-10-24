418d6b2bb70653a7b44369d68298c15b0a8686c0
samWidlund <sam.wid415@gmail.com>
2025-10-24 08:39:07 +0200

    set sandbox to false

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 EBAY_CLIENT_ID = os.getenv('EBAY_CLIENT_ID')
 EBAY_CLIENT_SECRET = os.getenv('EBAY_CLIENT_SECRET')
-EBAY_SANDBOX = True  # Change to False for production
+EBAY_SANDBOX = False  # Change to False for production
 
 if not EBAY_CLIENT_ID or not EBAY_CLIENT_SECRET:
     raise RuntimeError("Missing EBAY_CLIENT_ID or EBAY_CLIENT_SECRET in environment")
