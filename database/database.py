03ec470a3cf0dce11658db8f27f0ec1eebe328ab
samWidlund <sam.wid415@gmail.com>
2026-02-01 00:20:48 +0100

    make function to add_product to database

--- database/database.py
+++ database/database.py
@@ -7,18 +7,20 @@ supabase_url = os.environ.get('SUPABASE_URL')
 if not supabase_key or not supabase_url:
     raise ValueError("Missing supabase_key or supabase_url environment variables")
 
-response = requests.post(
-    f"{supabase_url}/rest/v1/products",
-    headers={
-        "apikey": supabase_key,
-        "Authorization": f"Bearer {supabase_key}",
-        "Content-Type": "application/json",
-        "Prefer": "resolution=merge-duplicates"
-    },
-    json={
-        "itemid": "1231313231",
-        "title": "Arcteryx beta lt jacket",
-        "price": "2999",
-        "url": "facebook.com"
-    }
-)
+
+def add_product(itemid: str, title: str, price: float, url: str):
+    response = requests.post(
+        f"{supabase_url}/rest/v1/products",
+        headers={
+            "apikey": supabase_key,
+            "Authorization": f"Bearer {supabase_key}",
+            "Content-Type": "application/json",
+            "Prefer": "resolution=merge-duplicates"
+        },
+        json={
+            "itemid": itemid,
+            "title": title,
+            "price": price,
+            "url": url
+        }
+    )
