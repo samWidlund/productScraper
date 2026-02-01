836b32edb49864c62826283a015d11764535eb68
samWidlund <sam.wid415@gmail.com>
2026-02-01 23:02:01 +0100

    commit all, need to fix supabase auth

--- database/database.py
+++ database/database.py
@@ -1,30 +1,37 @@
 import requests
+import supabase
+from supabase import auth
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-def add_product(itemid: str, title: str, price: float, url: str):
-    supabase_key = os.environ.get('SUPABASE_KEY')
-    supabase_url = os.environ.get('SUPABASE_URL')
+class SupabaseClient:
+    def __init__(self):
+        self.supabase_key = os.environ.get('SUPABASE_KEY')
+        self.supabase_url = os.environ.get('SUPABASE_URL')
 
-    if not supabase_key or not supabase_url:
-        raise ValueError("Missing supabase_key or supabase_url environment variables")
-    
-    response = requests.post(
-        f"{supabase_url}/rest/v1/products",
-        headers={
-            "apikey": supabase_key,
-            "Authorization": f"Bearer {supabase_key}",
-            "Content-Type": "application/json",
-            "Prefer": "resolution=merge-duplicates"
-        },
-        json={
+        if not self.supabase_key or not self.supabase_url:
+            raise ValueError("Missing supabase_key or supabase_url environment variables")
+        
+        self.client = supabase.create_client(self.supabase_url, self.supabase_key)
+
+    def add_product(self, itemid: str, title: str, price: float, url: str):
+
+        user = supabase.auth.get_user()  
+        user_id =  user['data']['user']['id']
+        # det funkade innan jag lade till rls, måste lösa med owner_id nu
+
+        response = self.client.table("products").insert({
             "itemid": itemid,
             "title": title,
             "price": price,
-            "url": url
-        }
-    )
+            "url": url,
+            "owner_id": user_id
+        }).execute()
+        return response
 
-    return response
+    def is_new_product(self, itemid: str):
+        response = self.client.table("products").select("*").eq("itemid", itemid).execute()
+        print(f"Database check for itemid {itemid}, found {len(response.data)} records.")
+        return len(response.data) == 0
--- fetch_fbMarketplace.py
+++ fetch_fbMarketplace.py
@@ -1,9 +1,8 @@
 import os
-import json
 from dotenv import load_dotenv
 from apify_client import ApifyClient
 from telegramBot import notify_product
-from database.database import add_product
+from database.database import SupabaseClient
 
 load_dotenv()
 APIFY_TOKEN=os.getenv("APIFY_TOKEN")
@@ -20,13 +19,15 @@ run_input = {
 # run actor
 run = client.actor("U5DUNxhH3qKt5PnCf").call(run_input=run_input)
 
-debug = False
+# initialize supabase client
+db = SupabaseClient()
 
 # fetch produkts n notify
 items = [] # debug
 for item in client.dataset(run["defaultDatasetId"]).iterate_items():
-    add_product(item['id'], item['marketplace_listing_title'], item['listing_price']['amount'], item['listingUrl'])
-
+    print(f"Found item: {item['marketplace_listing_title']} at {item['listing_price']['amount']}")
+    if db.is_new_product(item['id']):
+        db.add_product(item['id'], item['marketplace_listing_title'], item['listing_price']['amount'], item['listingUrl'])
     # notify user
 
     # check if product is already found, then not notify? think the add_product does not add duplicates anyway, but the notification part must be modified
