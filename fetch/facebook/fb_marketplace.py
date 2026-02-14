f1c892470869b8ea039afbd6c5f5510cd2944f3a
samWidlund <sam.wid415@gmail.com>
2026-02-14 12:33:43 +0100

    update facebook to match new table and currency fix, test again when apify tokens are restored

--- fetch/facebook/fb_marketplace.py
+++ fetch/facebook/fb_marketplace.py
@@ -38,9 +38,9 @@ for item in client.dataset(run["defaultDatasetId"]).iterate_items():
     print(f"Found item: {item['marketplace_listing_title']} at {item['listing_price']['amount']}")
     total_items += 1
 
-    if db.is_new_product(item['id']): # notify user and and product to db if new
-        db.add_product(item['id'], item['marketplace_listing_title'], item['listing_price']['amount'], item['listingUrl'])
-        notify_product(item['marketplace_listing_title'], item['listing_price']['amount'], item['listingUrl'])
+    if db.is_new_product("facebook_marketplace", item['id']): # notify user and and product to db if new
+        db.add_product("facebook_marketplace", item['id'], item['marketplace_listing_title'], item['listing_price'], item['amount'], item['listingUrl'])
+        notify_product(item['marketplace_listing_title'], item['listing_price'], item['amount'], item['listingUrl'])
         new_items += 1
 
 print(f"Total items found: {total_items}")
