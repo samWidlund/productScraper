a25d799e7b0369ef3119e0720139d0a430af318a
samWidlund <sam.wid415@gmail.com>
2026-02-14 11:52:17 +0100

    working blocket scraping

--- fetch/blocket/blocket_marketplace.py
+++ fetch/blocket/blocket_marketplace.py
@@ -6,40 +6,62 @@ sys.path.insert(0, str(Path(__file__).parent.parent.parent))
 
 import os
 from dotenv import load_dotenv
-from apify_client import ApifyClient
 from notification.telegramBot import notify_product
 from database.database import SupabaseClient
 from blocket_api import (
     BlocketAPI,
-    Category,
+    Category, ## optional category filter
     SortOrder,
-    CarColor,
-    CarModel,
-    CarSortOrder,
-    CarTransmission,
     Location,
 )
 
 ## inital supabase client
-# db = SupabaseClient()
-# db.login()
+db = SupabaseClient()
+db.login()
 
 # # counter variables
-# total_items = 0
-# new_items = 0
+total_items = 0
+new_items = 0
 
 ## blocket API client
-
-
 api = BlocketAPI()
+
+# all swedish region blocket locations
+BLEKINGE = Location.BLEKINGE
+DALARNA = Location.DALARNA
+GOTLAND = Location.GOTLAND
+GAVLEBORG = Location.GAVLEBORG
+HALLAND = Location.HALLAND
+JAMTLAND = Location.JAMTLAND
+JONKOPING = Location.JONKOPING
+KALMAR = Location.KALMAR
+KRONOBERG = Location.KRONOBERG
+NORRBOTTEN = Location.NORRBOTTEN
+SKANE = Location.SKANE
+STOCKHOLM = Location.STOCKHOLM
+SODERMANLAND = Location.SODERMANLAND
+UPPSALA = Location.UPPSALA
+VARMLAND = Location.VARMLAND
+VASTERBOTTEN = Location.VASTERBOTTEN
+VASTERNORRLAND = Location.VASTERNORRLAND
+VASTMANLAND = Location.VASTMANLAND
+VASTRA_GOTALAND = Location.VASTRA_GOTALAND
+OREBRO = Location.OREBRO
+OSTERGOTLAND = Location.OSTERGOTLAND
+
+all_locations = [
+    BLEKINGE, DALARNA, GOTLAND, GAVLEBORG, HALLAND, JAMTLAND, JONKOPING,
+    KALMAR, KRONOBERG, NORRBOTTEN, SKANE, STOCKHOLM, SODERMANLAND, UPPSALA,
+    VARMLAND, VASTERBOTTEN, VASTERNORRLAND, VASTMANLAND, VASTRA_GOTALAND,
+    OREBRO, OSTERGOTLAND
+]
 
 # search all of blocket
 response = api.search(
-    "jacka",
+    "Arcteryx",
     sort_order=SortOrder.PRICE_ASC,
-    locations=[Location.STOCKHOLM, Location.UPPSALA],
-    category=Category.FRITID_HOBBY_OCH_UNDERHALLNING,
-)
+    locations=all_locations
+    )
 
 # The actual products are in the 'docs' key
 products = response['docs']
@@ -58,4 +80,13 @@ for product in products:
     print(f"Location: {location}")
     print(f"ID: {product_id}")
     print(f"URL: {url}\n")
-    # total_items += 1
+    total_items += 1
+
+    if db.is_new_product(product_id): # notify user and and product to db if new
+        db.add_product(product_id, heading, amount, url)
+        notify_product(heading, amount, url)
+        new_items += 1
+
+print(f"Total items found: {total_items}")
+print(f"New items found: {new_items}")
+
