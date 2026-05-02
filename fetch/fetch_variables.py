70f410a022f84387d96fbf5830792f4aba4ca298
Samuel Widlund <sam.wid415@gmail.com>
2026-05-02 13:34:17 +0200

    Update fetch_variables.py

--- fetch/fetch_variables.py
+++ fetch/fetch_variables.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # fetch variables from worfklow .env, otherwise uses default value. Change the default value to config when running locally. 
-search_term = os.environ.get("SEARCH_TERM") or "salomon adv skin 12" 
-min_price_sek = int(os.environ.get("MIN_PRICE_SEK") or 500)
-max_price_sek = int(os.environ.get("MAX_PRICE_SEK") or 2000)
-min_price_usd = int(os.environ.get("MIN_PRICE_USD") or 50)
-max_price_usd = int(os.environ.get("MAX_PRICE_USD") or 200)
+search_term = os.environ.get("SEARCH_TERM") or "arcteryx" 
+min_price_sek = int(os.environ.get("MIN_PRICE_SEK") or 0)
+max_price_sek = int(os.environ.get("MAX_PRICE_SEK") or 1000)
+min_price_usd = int(os.environ.get("MIN_PRICE_USD") or 0)
+max_price_usd = int(os.environ.get("MAX_PRICE_USD") or 100)
